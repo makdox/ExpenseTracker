01ff07ee39c44ec428f8aac0df9a984ed0c1a3a8
Maksym Donets <timnirton7@gmail.com>
2022-10-19 14:19:51 -0400

    Transaction Cumulative Sum

--- ExpenseTracker/ContentView.swift
+++ ExpenseTracker/ContentView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 import SwiftUICharts
 
 struct ContentView: View {
-    var demoData: [Double] = [8, 2, 4, 6, 12, 9, 2]
+    @EnvironmentObject var transactionListVM: TransactionListViewModel
+//    var demoData: [Double] = [8, 2, 4, 6, 12, 9, 2]
     var body: some View {
         NavigationView {
             ScrollView {
@@ -20,18 +21,22 @@ struct ContentView: View {
                         .bold()
                     
                     //MARK: Chart
-                    CardView {
-                        VStack {
-                            ChartLabel("900$", type: .title)
-                            
-                            LineChart()
-                        }
-                        .background(Color.systemBackground)
-                    }
-                    .data(demoData)
-                    .chartStyle(ChartStyle(backgroundColor: Color.systemBackground, foregroundColor: ColorGradient(Color.icon.opacity(0.4), Color.icon)))
-                    .frame(height: 300)
+                    let data = transactionListVM.accumulateTransaction()
                     
+                    if !data.isEmpty {
+                        let totalExpenses = data.last?.1 ?? 0
+                        CardView {
+                            VStack(alignment: .leading) {
+                                ChartLabel(totalExpenses.formatted(.currency(code: "USD")), type: .title, format: "$%.02f")
+                                
+                                LineChart()
+                            }
+                            .background(Color.systemBackground)
+                        }
+                        .data(data)
+                        .chartStyle(ChartStyle(backgroundColor: Color.systemBackground, foregroundColor: ColorGradient(Color.icon.opacity(0.4), Color.icon)))
+                        .frame(height: 300)
+                    }
                     
                     //MARK: Transaction List
                     RecentTransactionList()
